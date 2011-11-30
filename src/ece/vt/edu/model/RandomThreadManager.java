069429b497cd48b1e1d852f20c9673ee42576755
Kelson Gent <gent.kelson@gmail.com>
2011-11-30 13:55:14 -0500

    Minor Change, The threading model as well as the state class working as intended though
    @jforsyth

--- src/ece/vt/edu/model/RandomThreadManager.java
+++ src/ece/vt/edu/model/RandomThreadManager.java
@@ -6,7 +6,6 @@ public class RandomThreadManager extends ThreadManager{
     		public static final int NUM_THREADS = 100;
     		public static final int NUM_ITER = 1; 
     		
-		
 		ArrayList<State> best_states = new ArrayList<State>();
 		//build the ability to launch many threads from this manager. 
 		//Not entirely necessary just nice to have it contained.
